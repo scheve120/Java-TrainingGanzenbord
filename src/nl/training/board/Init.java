a9acc8179d97df9e8c400ca6419c218167b20a7a
David Berg <remotec@live.nl>
2020-03-22 19:36:16 +0100

    De basis voor het samen brengen van speler en bord is gedaan. Nu de interactiviteid toe brengen en als laatste de spel regels toe passen.

--- src/nl/training/board/Init.java
+++ src/nl/training/board/Init.java
@@ -1,15 +1,5 @@
 package nl.training.board;
 
-import sun.audio.AudioPlayer;
-import sun.audio.AudioStream;
-
-import javax.swing.*;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.Scanner;
-
 public class Init {
 
     public static void main(String[] args) {
--- src/nl/training/board/Spel.java
+++ src/nl/training/board/Spel.java
@@ -1,17 +1,17 @@
 package nl.training.board;
 
+import java.util.Scanner;
+
 public class Spel {
     Board bord = new Board();
     Dobbelsteen dobbelsteen = new Dobbelsteen();
     Speler speler1 = new Speler(); // Met meerdere spelers kan dit een array worden
 
-    void SpelerPositie() {
-//        bord.boardPosities.set(4, "*");
-        // Hier wordt de locatie van de spelers bepaald.
-        Dobbelsteen gooit = new Dobbelsteen();
-//        gooit.maakDobbelsteen();
-//        System.out.println("Dobbelsteen 1 = (" + gooit.dobbelsteen1 + ") Dobbelsteen 2 = (" + gooit.dobbelsteen2 + ") Totaal = (" + gooit.totaal + ")");
-//        System.out.println(gooit.dobbelsteen2);
+    void SpelerPositie(int worpen) {
+        System.out.println("Even kijken wat voor waarde hier in zit!! Strings of primitives?   " + speler1.positieSpeler += + worpen);
+        System.out.println(speler1 + "Gooit (" + dobbelsteen.dobbelsteen1+ ") (" + dobbelsteen.dobbelsteen2 + ") Totaal = (" + dobbelsteen.gooiMetTweeDobbelstenen() + ")");
+        System.out.println(bord.boardPosities.get(speler1.positieSpeler).positie);
+//        switch ()
     }
 
     void spelen() {
@@ -23,38 +23,21 @@ public class Spel {
     private void speelRonde() { // indexe uit spelersarray meegeeft.
         // gooit
         int worpen = dobbelsteen.gooiMetTweeDobbelstenen();
+        boolean stop = false;
+        do {
+            String sepelerActie = speler1.inputController().nextLine();
+            if (sepelerActie.equals("Gooi")) {
+                SpelerPositie(worpen);
+            } else if (sepelerActie.equals("Stop")) {
+                stop = false;
+            } else {
+                System.out.println("Als je wil stoppen met je zeggen 'Stop' ");
+            }
+        } while (!stop);
 
-        /*
-        System.out.println("gegooid: " + worpen);
-//        worpen = 5; // even testen
-        speler1.positieSpeler += + worpen;
-        System.out.println("sta nu op plek: " + bord.boardPosities.get(speler1.positieSpeler).positie + " " + bord.boardPosities.get(speler1.positieSpeler).specialePositie);
-//        worpen = 14; // even testen
-        worpen = dobbelsteen.gooiMetTweeDobbelstenen(); // even testen
-        speler1.positieSpeler += + worpen;
-        System.out.println("gegooid: " + worpen);
-        System.out.println("sta nu op plek: " + bord.boardPosities.get(speler1.positieSpeler).positie + " " + bord.boardPosities.get(speler1.positieSpeler).specialePositie);
-//        worpen = 1; // even testen
-        worpen = dobbelsteen.gooiMetTweeDobbelstenen(); // even testen
-        speler1.positieSpeler += + worpen;
-        System.out.println("gegooid: " + worpen);
-        System.out.println("sta nu op plek: " + bord.boardPosities.get(speler1.positieSpeler).positie + " " + bord.boardPosities.get(speler1.positieSpeler).specialePositie);
         // naar nieuw plek => positie updaten
         // kijken wat de positie inhoudt
         // eventueel opdracht positie uitvoer
-         */
 
-        boolean stop = true;
-        speler1.inputController();
-
-        do {
-            if (speler1.inputController().equals("Gooi")) {
-                System.out.println(speler1 + "Gooit " + dobbelsteen.dobbelsteen1+ " " + dobbelsteen.dobbelsteen2);
-            } else if (speler1.inputController().equals("Stop")) {
-                stop = true;
-            } else {
-                stop = false;
-            }
-        } while (stop == true);
     }
 }
